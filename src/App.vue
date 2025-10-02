281347f16cdb75202729b3a7ea34d0c4d3e01232
zhaokuang <zhaokuangDK@163.com>
2025-10-02 22:24:02 +0800

    fix:把所有全局样式移动到style.css中

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,5 @@
 </script>
 
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <router-view />
 </template>
-
-<style>
-* {
-  margin: 0;
-  padding: 0;
-}
-
-#app {
-  width: 100%;
-  height: 100%;
-}
-
-html, body {
-  width: 100vh;
-  height: 100vh;
-  overflow: hidden;
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import './style.css'
 import router from './router'
 
 const app = createApp(App)
